282809fd900b7f6437fbf539fa2cf1aff7a11514
damoglumuratevren <60298354+damoglumuratevren@users.noreply.github.com>
2022-06-11 11:35:10 +0300

    First Commit
    Proje oluşturuldu. Datatable eklendi

---
+++ innovateKUYS/innovateKUYS/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    $('#student').DataTable({
+        columnDefs: [
+            {
+                targets: [0],
+                orderData: [0, 1],
+            },
+            {
+                targets: [1],
+                orderData: [1, 0],
+            },
+            {
+                targets: [4],
+                orderData: [4, 0],
+            },
+        ],
+    });
+});
